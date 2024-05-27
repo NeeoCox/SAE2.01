a25b9172d55cf8925497f912402173542a27a375
NeeoCox <maelcoignard16@gmail.com>
2024-05-27 10:45:44 +0200

    modified:   class/view/Accueil$Header.class 	modified:   class/view/Accueil.class 	modified:   src/view/Accueil.java

--- src/view/Accueil.java
+++ src/view/Accueil.java
@@ -1,6 +1,8 @@
 package view;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -34,8 +36,14 @@ public class Accueil extends BorderPane {
 		private Button loginBtn;
 		private ImageView imageView;
 		private Image image;
+		private GridPane pane;
+		private GridPane paneBar;
 
 		public Header(){
+			pane = new GridPane();
+			paneBar = new GridPane();
+			paneBar.setAlignment(Pos.CENTER);
+			paneBar.setHgap(5);
 			/*String nomImg = "C:/Users/Administrateur/Desktop/Dossier_avec_tout/BUTnfo/SAE/Gros_SAE/Java/img/pikmin.png";
 			imageView = new ImageView();
 
@@ -52,12 +60,13 @@ public class Accueil extends BorderPane {
 			searchBar = new TextField();
 			searchBtn = new Button("Search");
 			loginBtn = new Button("Login");
+
 			this.add(exportBtn,1,0);
 			this.add(searchBar,2,0);
 			this.add(searchBtn,3,0);
 			this.add(loginBtn,4,0);
 
-
+			
 
 		}
 
