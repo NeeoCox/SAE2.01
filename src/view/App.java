717bf93e1ad796be4c1c5e0bf0bc027c2497d510
NeeoCox <maelcoignard16@gmail.com>
2024-05-28 12:54:02 +0200

    renamed:    BasicApplication_css/BasicApplication.css -> ressources/BasicApplication.css 	renamed:    BasicApplication_css/BasicApplication_css.fxml -> ressources/BasicApplication_css.fxml 	modified:   src/view/App.java 	new file:   ws/IHM_BUT1.pdf

--- src/view/App.java
+++ src/view/App.java
@@ -1,21 +1,28 @@
 package view;
+import java.io.IOException;
+
 import controller.*;
 import model.dao.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	@Override
 	public void start(Stage stage){
-		View _view = new View();
+		try{
+			Parent root = FXMLLoader.load(getClass().getResource("/BasicApplication_css.fxml"));
+			stage.setTitle("SAE commune bretonne");
+			stage.setScene(new Scene(root, 500,500));
+			stage.show();
+		}
+		catch(IOException e){
+			
+		}
 		
-		Controller _controller = new Controller(_view);
-		/*
-		DAO d = new DAO();
-		d.query("SELECT * FROM Gare");
-		*/
-
-		_view.show();
+		
 	}
 
 	public static void main(String[] args) {
