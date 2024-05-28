3cf90274e460a70e005aa1df8ae5561bd2398dc1
Patrique29 <baka.coca@gmail.com>
2024-05-28 10:45:33 +0200

    modified:   class/controller/Controller.class 	modified:   class/model/dao/DAO.class 	modified:   class/view/Accueil$Header.class 	modified:   class/view/Accueil.class 	modified:   class/view/App.class

--- src/view/App.java
+++ src/view/App.java
@@ -5,22 +5,25 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class App extends Application {
 	public void start(Stage stage){
 		View _view = new View();
 		
 		Controller _controller = new Controller(_view);
-		/*
-		DAO d = new DAO();
-		d.query("SELECT * FROM Gare");
-		*/
-
+		
+		DAO d = new DAO("jdbc:mysql://localhost:3306/commune");
 		_view.show();
 	}
-
 	public static void main(String[] args) {
 		launch(args);
 
 		
 	}
+
+
+
 }
