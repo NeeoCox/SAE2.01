4f8cacd991d37cd49d26202e9919721c55a117a4
Delecons <noe.parcollet@gmail.com>
2024-05-28 17:33:18 +0200

    new file:   TestMethode.java 	new file:   lib/hamcrest-core-1.3.jar 	new file:   lib/junit-4.11-javadoc.jar 	new file:   lib/junit-4.11.jar 	modified:   src/model/data/Aeroport.java 	modified:   TestMethode.java

---
+++ TestMethode.java
@@ -0,0 +1,14 @@
+import model.dao.*;
+import org.junit.*;
+import org.junit.runner.*;
+import static org.junit.Assert.*;
+/**
+ * 
+ */
+public class TestMethode{
+    
+    @Test
+    public void testCheckAnnee(){
+        
+    }
+}
