eb0bfbc04bf677ff9fa9950dd4449003793d1196
Patrique29 <baka.coca@gmail.com>
2024-05-28 16:22:16 +0200

    modified:   class/model/dao/DAO.class 	modified:   class/model/data/Scenario.class 	modified:   class/view/App.class 	modified:   src/model/data/Scenario.java 	modified:   src/view/App.java

--- src/view/App.java
+++ src/view/App.java
@@ -5,6 +5,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import model.data.*;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -15,7 +17,10 @@ public class App extends Application {
 		
 		Controller _controller = new Controller(_view);
 		
-		DAO d = new DAO("jdbc:mysql://localhost:3306/commune");
+		Scenario.testDAO();
+
+		
+		
 		_view.show();
 	}
 	public static void main(String[] args) {
