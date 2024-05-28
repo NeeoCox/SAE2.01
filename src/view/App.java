26fb12f97dd85aefc5962404f7ca217abeb50aa7
NeeoCox <maelcoignard16@gmail.com>
2024-05-28 15:28:44 +0200

    modified:   class/view/App.class 	renamed:    ressources/BasicApplication.css -> class/view/BasicApplication.css 	renamed:    ressources/BasicApplication_css.fxml -> class/view/BasicApplication_css.fxml 	renamed:    ressources/img/Bossman_Hero_29.webp -> class/view/img/Bossman_Hero_29.webp 	renamed:    ressources/img/free-nature-images.jpg -> class/view/img/free-nature-images.jpg 	renamed:    ressources/img/pikmin.png -> class/view/img/pikmin.png 	modified:   src/view/App.java 	modified:   ws/c.bat

--- src/view/App.java
+++ src/view/App.java
@@ -1,33 +1,32 @@
 package view;
-import java.io.IOException;
-
-import controller.*;
-import model.dao.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
 
 public class App extends Application {
-	@Override
-	public void start(Stage stage){
-		try{
-			Parent root = FXMLLoader.load(getClass().getResource("/BasicApplication_css.fxml"));
-			stage.setTitle("SAE commune bretonne");
-			stage.setScene(new Scene(root, 500,500));
-			stage.show();
-		}
-		catch(IOException e){
-			
-		}
-		
-		
-	}
+    @Override
+    public void start(Stage stage) {
+        try {
+            URL fxmlLocation = getClass().getClassLoader().getResource("BasicApplication_css.fxml");
+            if (fxmlLocation == null) {
+                System.out.println("FXML file not found!");
+            } else {
+                Parent root = FXMLLoader.load(fxmlLocation);
+                stage.setTitle("SAE commune bretonne");
+                stage.setScene(new Scene(root, 500, 500));
+                stage.show();
+            }
+        } catch (IOException e) {
+            System.out.println("Potit probleme");
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-
-		
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
