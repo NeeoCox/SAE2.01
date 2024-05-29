64c2422fcd7bb9b30ef6f1fadb0a24ca26263da1
Patrique29 <baka.coca@gmail.com>
2024-05-29 11:03:42 +0200

    Ajouter la base pour la connexion

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,15 +3,23 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.swing.Action;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import model.dao.CommuneDAO;
 import model.data.Commune;
 import view.*;
+import javafx.scene.*;
+import javafx.scene.paint.Color;
 /**
  * Controller
  */
@@ -25,6 +33,12 @@ public class Controller {
 	Button recherche;
 	@FXML
 	Button connexion;
+	@FXML
+	TextField mail;
+	@FXML
+	TextField mdp;
+	@FXML
+	Label labelIncorrect;
 
 	CommuneDAO _c;
 
@@ -38,10 +52,36 @@ public class Controller {
 
 
 	public void recherche(ActionEvent e){
-		System.out.println("ok");
 		if(villeAChercher == null ||villeAChercher.getText().length() == 0) throw new IllegalArgumentException("Bar de recherche vide");
 		ArrayList<Commune> a = _c.find(villeAChercher.getText().toUpperCase());
 		System.out.println(a.size());
+	}
+
+
+	public void versPageLogin(ActionEvent e){
+		System.out.println("login");
+		Scene scene =null;
+		try {
+			Parent root = FXMLLoader.load(new URL("file:../ressources/Login.fxml"));
+			scene= new Scene(root , 600, 500);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}		
+		Stage stage= (Stage) ((Node)e.getSource ()).getScene ().getWindow ();
+		stage.setScene(scene);
+
+	}
+
+	public void connecter(ActionEvent e){
+		//todo avoir des gens pour ce co
+		if(!mail.getText().equals("admin") || !mdp.getText().equals("admin")) {
+			labelIncorrect.setTextFill(Color.rgb(255, 0, 0));
+			labelIncorrect.setText("identifiant ou mot de passe invalide");
+			System.out.println("pas ok");
+		}
+		else System.out.println("ok");
+		System.out.println(mail.getText());
+		System.out.println(mdp.getText());
 	}
 
 
